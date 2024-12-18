fc35fc2c5bf8640276376f640c4a265ece0c9d57
Koleen BP - Deloitte <KPaunon@deloitte.com>
2024-12-18 14:28:09 +0800

    Addressing imports sequencing

--- apps/next-app/src/app/layout.tsx
+++ apps/next-app/src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+
 import { MainNav } from "@/components/MainNav";
+
+import "./globals.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
