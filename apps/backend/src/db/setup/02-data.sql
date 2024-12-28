73237edae6102b64a4049c76bac6c32dbed34331
mestaciodel <mestacio@deloitte.com>
2024-12-28 20:44:54 +0800

    update initial database scripts

--- apps/backend/src/db/setup/02-data.sql
+++ apps/backend/src/db/setup/02-data.sql
@@ -1,6 +1,119 @@
-INSERT INTO "public"."learning_resource_type" ("id", "name", "description") VALUES ('1', 'Digital Learning', 'Online learning'), ('2', 'Classroom', 'In person classroom training'), ('3', 'Virtual Classroom', 'Online classroom training');
-INSERT INTO "public"."learning_resource" ("id", "type_id", "name", "url", "description", "deadline_at", "is_mandatory", "created_at") VALUES ('1', '2', 'Ethics, Integrity and Sexual Harassment Training', 'www.abc.com', 'Classroom type training', '2024-12-31 15:55:00+00', 'true', '2024-12-17 03:41:36+00'), ('2', '1', 'DCPDC Privacy Policy 2024', 'www.abc.com', 'Online video', '2024-12-31 04:55:00+00', 'true', '2024-12-17 03:46:01+00'), ('3', '1', '2024 Global Anti-Corruption and Financial Crime Compliance Refresher', 'www.abc.com', 'Online video', '2024-12-31 04:55:00+00', 'true', '2024-12-17 03:47:28+00'), ('4', '3', 'Next.js Conference', 'www.abc.com', 'Online conference', null, 'false', '2024-12-17 03:49:32.527341+00');
-INSERT INTO "public"."learning_status" ("id", "name", "description") VALUES ('1', 'Not Started', null), ('2', 'In Progress', null), ('3', 'Completed', null);
-INSERT INTO "public"."user_role" ("id", "name", "description") VALUES ('2', 'manager', 'Lead role'), ('3', 'Individual Contributor', 'IC Role');
-INSERT INTO "public"."user" ("id", "role_id", "first_name", "last_name", "email", "is_active") VALUES ('1', '2', 'Marvin', 'Martian', 'martian@mars.com', 'true');
-INSERT INTO "public"."assigned_learning_resource" ("id", "user_id", "resource_id", "status_id", "started_at", "completed_at", "modified_at", "created_at") VALUES ('1', '1', '1', '1', null, null, null, '2024-12-17 10:07:57.070794+00'), ('3', '1', '2', '1', null, null, null, '2024-12-17 10:08:16.971748+00'), ('4', '1', '3', '1', null, null, null, '2024-12-17 10:08:29.038059+00'), ('5', '1', '4', '1', null, null, null, '2024-12-17 10:08:49.032982+00');
+-- Dummy data
+INSERT INTO "public"."learning_resource_type"
+            ("name",
+             "description")
+VALUES      ('Digital Learning',
+             'Online learning'),
+            ('Classroom',
+             'In person classroom training'),
+            ('Virtual Classroom',
+             'Online classroom training'); 
+
+INSERT INTO "public"."learning_resource"
+            ("type_id",
+             "name",
+             "url",
+             "description",
+             "deadline_at",
+             "is_mandatory",
+             "created_at")
+VALUES      ('2',
+             'Ethics, Integrity and Sexual Harassment Training',
+             'www.abc.com',
+             'Classroom type training',
+             '2024-12-31 15:55:00+00',
+             'true',
+             ( Now() AT TIME zone 'utc' :: text )),
+            ('1',
+             'DCPDC Privacy Policy 2024',
+             'www.abc.com',
+             'Online video',
+             '2024-12-31 04:55:00+00',
+             'true',
+             ( Now() AT TIME zone 'utc' :: text )),
+            ('1',
+             '2024 Global Anti-Corruption and Financial Crime Compliance Refresher',
+             'www.abc.com',
+             'Online video',
+             '2024-12-31 04:55:00+00',
+             'true',
+             ( Now() AT TIME zone 'utc' :: text )),
+            ('3',
+             'Next.js Conference',
+             'www.abc.com',
+             'Online conference',
+             NULL,
+             'false',
+             ( Now() AT TIME zone 'utc' :: text )); 
+
+INSERT INTO "public"."learning_status"
+            ("name",
+             "description")
+VALUES      ('Not Started',
+             'User has not not started the learning resource.'),
+            ('In Progress',
+             'User has started the learning resource but has not completed it yet.'),
+            ('Completed',
+             'User has completed the learning resource.'); 
+
+INSERT INTO "public"."user_role"
+            ("name",
+             "description")
+VALUES      ('Manager',
+             'Lead role'),
+            ('Individual Contributor',
+             'IC Role'); 
+
+INSERT INTO "public"."user"
+            ("role_id",
+             "first_name",
+             "last_name",
+             "email",
+             "is_active")
+VALUES      ('1',
+             'Marvin',
+             'Martian',
+             'martian@mars.com',
+             'true'); 
+
+INSERT INTO "public"."assigned_learning_resource"
+            (
+             "user_id",
+             "resource_id",
+             "status_id",
+             "started_at",
+             "completed_at",
+             "modified_at",
+             "created_at")
+VALUES      (
+             '1',
+             '1',
+             '1',
+             NULL,
+             NULL,
+             NULL,
+             (now() AT TIME ZONE 'utc'::text)),
+            (
+             '1',
+             '2',
+             '1',
+             NULL,
+             NULL,
+             NULL,
+             (now() AT TIME ZONE 'utc'::text)),
+            (
+             '1',
+             '3',
+             '1',
+             NULL,
+             NULL,
+             NULL,
+             (now() AT TIME ZONE 'utc'::text)),
+            (
+             '1',
+             '4',
+             '1',
+             NULL,
+             NULL,
+             NULL,
+             (now() AT TIME ZONE 'utc'::text)); 
