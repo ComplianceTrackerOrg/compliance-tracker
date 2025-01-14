ce645deb23559cd5848039969991399980c41466
Koleen BP - Deloitte <KPaunon@deloitte.com>
2025-01-14 17:29:23 +0800

    FEAT-SB: Adding login component & feature on Storybook

--- apps/next-app/src/features/login/stories/EmailForm.stories.tsx
+++ apps/next-app/src/features/login/stories/EmailForm.stories.tsx
@@ -1,19 +1,18 @@
-import { Meta, StoryObj } from '@storybook/react'
+import { Meta, StoryObj } from "@storybook/react"
 
-import { EmailForm } from '../components/EmailForm';
+import { EmailForm } from "../components/EmailForm"
 
 const meta: Meta<typeof EmailForm> = {
-    title: "Components/Login/Sub-components/EmailForm",
-    component: EmailForm,
-    parameters: {
-        layout: "centered",
-    },
-    
+  title: "Components/Login/Sub-components/EmailForm",
+  component: EmailForm,
+  parameters: {
+    layout: "centered",
+  },
 }
 
-export default meta;
+export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-    name: "Email form",   
+  name: "Email form",
 }
