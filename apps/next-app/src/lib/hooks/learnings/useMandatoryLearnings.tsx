43ba717e75447d4c9d1830371dbe5c95f5a03d43
mestaciodel <mestacio@deloitte.com>
2025-01-15 15:08:19 +0800

    feature: initial implementation of graphql in nextjs, includes using codegen

---
+++ apps/next-app/src/lib/hooks/learnings/useMandatoryLearnings.tsx
@@ -0,0 +1,18 @@
+import { useQuery } from "@urql/next"
+import { GetAllTrainings } from "@/lib/graphql/queries"
+
+export const useTrainings = (isMandatory?: boolean) => {
+  const [result] = useQuery({
+    query: GetAllTrainings.toString(),
+    variables: {
+      isMandatory: isMandatory,
+    },
+  })
+  const { data, error, fetching } = result
+
+  return {
+    data: data?.learning_resourceCollection?.edges,
+    error,
+    fetching,
+  }
+}
