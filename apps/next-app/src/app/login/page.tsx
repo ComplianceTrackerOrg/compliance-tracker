535bebbc75ac49dbba0c7e5409f8588ddacd27ea
jescultura <esculturajerome@gmail.com>
2025-01-16 10:56:47 +0800

    address PR comments

--- apps/next-app/src/app/login/page.tsx
+++ apps/next-app/src/app/login/page.tsx
@@ -1,8 +1,7 @@
 import { Metadata } from "next"
 import Link from "next/link"
 
-import { cn } from "@/lib/utils"
-import { UserAuthForm } from "./components/user-auth-form"
+import { UserAuthForm } from "../../features/login/components/user-auth-form"
 
 export const metadata: Metadata = {
   title: "Authentication",
--- apps/next-app/src/features/login/components/user-auth-form.tsx
+++ apps/next-app/src/features/login/components/user-auth-form.tsx
@@ -3,13 +3,9 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
-// import { Icons } from "@/components/icons"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-// import { Button } from "@/registry/new-york/ui/button"
-// import { Input } from "@/registry/new-york/ui/input"
-// import { Label } from "@/registry/new-york/ui/label"
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
