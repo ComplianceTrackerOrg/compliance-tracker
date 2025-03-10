e2e0d79ef592b3303525ca07c90c56bf79c90ebe
mestaciodel <mestacio@deloitte.com>
2025-03-10 18:44:23 +0800

    feature: implement initial loading UI

---
+++ apps/next-app/src/components/ui/skeleton/index.ts
@@ -0,0 +1,4 @@
+export * from "./skeleton"
+export { default as SkeletonManageUsers } from "./skeleton-manage-users"
+export { default as SkeletonManageRequirements } from "./skeleton-manage-requirements"
+export { default as SkeletonManageTrainings } from "./skeleton-manage-trainings"
