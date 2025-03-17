b2a7d1e81c3f2e7dba59c750becce1a403e67822
Wilphred Paul Enriquez <wenriquez@deloitte.com>
2025-03-17 11:00:52 +0800

    fix: angular-123 Added missing components to my trainings page

--- apps/angular-app/src/app/features/trainings/my-trainings/my-trainings.component.ts
+++ apps/angular-app/src/app/features/trainings/my-trainings/my-trainings.component.ts
@@ -11,13 +11,19 @@ import {
   HlmSpinnerModule,
   HlmSpinnerComponent,
 } from '@spartan-ng/ui-spinner-helm';
-import { ButtonComponent } from '~shared/components/ui/button/button.component';
 
 import { TrainingsService } from '../trainings.service';
 import {
   AssignedLearningResourceData,
   TrainingData,
 } from '../../../shared/components/models/trainings.model';
+import { BrnSelectImports } from '@spartan-ng/brain/select';
+import { HlmSelectImports } from '@/libs/ui/ui-select-helm/src';
+import {
+  BrnProgressComponent,
+  BrnProgressIndicatorComponent,
+} from '@spartan-ng/brain/progress';
+import { HlmProgressIndicatorDirective } from '@/libs/ui/ui-progress-helm/src';
 
 @Component({
   selector: 'app-my-trainings',
@@ -27,17 +33,33 @@ import {
     HlmThComponent,
     HlmTdComponent,
     HlmTrowComponent,
-    ButtonComponent,
     HlmSpinnerModule,
     HlmSpinnerComponent,
+    BrnProgressComponent,
+    BrnProgressIndicatorComponent,
+    HlmProgressIndicatorDirective,
+    BrnSelectImports,
+    HlmSelectImports,
   ],
   templateUrl: './my-trainings.component.html',
 })
 export class MyTrainingsComponent implements OnInit {
   myTrainingsData: TrainingData[] = [];
+  trainingsTotal = 0;
+  trainingsCompleted = 0;
   loading: boolean = true;
+  options = [
+    { name: 'Not Started', label: 'Mark as Not Started', value: 1 },
+    { name: 'In Progress', label: 'Mark as In Progress', value: 2 },
+    { name: 'Completed', label: 'Mark as Completed', value: 3 },
+  ];
 
   constructor(private trainingsService: TrainingsService) {}
+
+  get progress() {
+    if (this.trainingsTotal === 0) return 0;
+    return (this.trainingsCompleted / this.trainingsTotal) * 100;
+  }
 
   ngOnInit(): void {
     // TODO: make passed id dynamic
@@ -45,15 +67,22 @@ export class MyTrainingsComponent implements OnInit {
     if (userId) {
       this.trainingsService.getMyTrainings(userId).subscribe({
         next: (data: AssignedLearningResourceData[]) => {
+          let trainingsCount = 0;
+          let completedTrainingsCount = 0;
           this.myTrainingsData =
             data &&
             data.map((item) => {
+              trainingsCount += 1;
               const { node } = item;
               const {
                 learning_resource: resource,
                 resource_status: status,
                 id,
               } = node;
+
+              if (status.name.toLowerCase() === 'completed') {
+                completedTrainingsCount += 1;
+              }
 
               return {
                 id,
@@ -64,6 +93,8 @@ export class MyTrainingsComponent implements OnInit {
                 trainingUrl: resource.url,
               };
             });
+          this.trainingsTotal = trainingsCount;
+          this.trainingsCompleted = completedTrainingsCount;
           this.loading = false;
         },
         error: (err) => {
