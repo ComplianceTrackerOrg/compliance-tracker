964d876ba11d91622d9738750f0483b09ae088cf
jesse <jdino@deloitte.com>
2025-03-21 07:35:32 +0800

    CHORE[NEXTJS] - Add vitest-ui coverage thresholds (#134)
    * feature: applied more toast messages and zod rule update (#133)
    
    * feature(next-app): add vitest and react-testing library
    
    * feature(next-app): add vitest and react-testing library
    
    * refactor(next-app): change vitest config file name
    
    * test(next-app): add vitest-ui
    
    * chore(next-app): remove html for unit test
    
    * chore(next-app): update /html/ in ignore
    
    * chore(next-app): add *.gz in gitignore to not include those files when vitest-ui runs
    
    * CHORE[NEXTJS]: remove spec files in components then move test cases for feature folder
    
    ---------
    
    Co-authored-by: Marvin Estacio <85913753+mestaciodel@users.noreply.github.com>

--- apps/next-app/vitest.config.ts
+++ apps/next-app/vitest.config.ts
@@ -1,15 +1,36 @@
 import { defineConfig } from "vitest/config"
-import { defineConfig as viteDefineConfig } from "vite"
-import tsconfigPaths from "vite-tsconfig-paths"
 import react from "@vitejs/plugin-react"
+import path from "path"
 
-export default defineConfig(
-  viteDefineConfig({
-    plugins: [tsconfigPaths(), react()],
-    test: {
-      globals: true,
-      environment: "jsdom",
-      include: ["**/*.spec.tsx"],
-    },
-  })
+import tsconfig from "./tsconfig.json"
+
+const alias = Object.fromEntries(
+  Object.entries(tsconfig.compilerOptions.paths).map(([key, [value]]) => [
+    key.replace("/*", ""),
+    path.resolve(__dirname, value.replace("/*", "")),
+  ])
 )
+
+export default defineConfig({
+  resolve: {
+    alias,
+  },
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    include: ["src/features/**/*.spec.tsx"],
+    exclude: ["node_modules"],
+    coverage: {
+      enabled: true,
+      include: ["src/features/**/*.tsx"],
+      thresholds: {
+        lines: 80,
+        statements: 80,
+        branches: 80,
+        functions: 80,
+      },
+    },
+    reporters: ["default", "html"],
+  },
+})
